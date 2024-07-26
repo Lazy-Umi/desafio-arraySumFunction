ae4b3a88872b38e292a4c9c09348ec7cbc347876
Lazy-Umi <202308009702@alunos.estacio.br>
2024-07-26 12:30:32 -0300

    Desafio funções

---
+++ src/functionsChallenge.js
@@ -0,0 +1,18 @@
+/*
+    Criar uma função que recebe como argumento um array de inteiros e um numero inteiro alvo. 
+    A função deve retornar os indices que somandos é igual o valor alvo. Por exemplo:
+    
+    array = [1 ,3 ,5, 7, 6, 10]
+    alvo = 11
+
+    saida: [0, 5]
+*/
+
+function calcArray(array, target) {
+    for(let i = 0; i < array.length; i++){
+        console.log(array[5])        
+    }
+
+}
+let array = [1, 3, 5, 7, 6, 10]
+calcArray(array)
